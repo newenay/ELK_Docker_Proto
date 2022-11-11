72219847d4491a49b4ae4157aa970fafb5bbd83d
newenay <newenay@gmail.com>
2022-11-11 15:47:06 -0500

    Add Appsetting JSON Writer

---
+++ SimpleClient/CreateJSON.cs
@@ -0,0 +1,15 @@
+namespace CreateJSON
+{
+    public class CreateLocalJSON
+    {
+        public string? FirstName { get; set;}
+        public string? LastName { get; set;}
+        public int employeeID { get; set;}
+        public string? Designation { get; set;}
+    }
+
+    public class CreatePipelineJSON
+    {
+       // Add a Pipeline set 
+    }
+}
--- SimpleClient/Program.cs
+++ SimpleClient/Program.cs
@@ -1,6 +1,9 @@
 using Serilog;
 using Serilog.Sinks.Elasticsearch;
 using TestELK8;
+using CreateJSON;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +27,42 @@ builder.Host.UseSerilog((hostBuilder, loggerConfiguration) =>
             TypeName = null,
             BatchAction = ElasticOpType.Create
         });
+
+    string checkForAppSettings = @".\appsettings.Local.json";
+    //Console.WriteLine(File.Exists(checkForAppSettings) ? "File exists." : "File does not exist.");
+    if( File.Exists(checkForAppSettings) ) {
+        Console.WriteLine("File exists.");
+    }
+    else
+    {
+        Console.WriteLine("Need to create the file");
+        CreateNewJSON();
+    }
 });
+
+static void CreateNewJSON(){
+    // May need switcher to write different types of appSettings
+    CreateLocalJSON newJSON = new CreateLocalJSON
+    {
+        FirstName = null, 
+        LastName = null,
+        employeeID = 0,
+        Designation = null 
+    };
+
+    string JSONresult = JsonSerializer.Serialize(newJSON);
+
+    string path = @".\appsettings.Local.json";
+
+    using (var tw = new StreamWriter(path, true))
+    {
+        tw.WriteLine(JSONresult.ToString());
+        tw.Close();
+    }
+
+
+
+}
 
 var app = builder.Build();
 
